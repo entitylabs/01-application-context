e6561f6dc5feaf024deb44b9071a2edd5aa3c1c5
entitylabs <entitylabs@gmail.com>
2021-11-13 18:31:49 +0100

    Added web context code

--- 03-webconfig-based-application-context/src/main/java/org/entitylabs/Application.java
+++ 03-webconfig-based-application-context/src/main/java/org/entitylabs/Application.java
@@ -1,17 +1,31 @@
 package org.entitylabs;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-public class Application {
+public class Application implements WebApplicationInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		
+		var applicationContext= new AnnotationConfigWebApplicationContext();
+			applicationContext.registerShutdownHook();
+			
+			applicationContext.setServletContext(servletContext);
+			applicationContext.register(ApplicationConfiguration.class);
+			applicationContext.refresh();
 
-		/**
-		 * scanning a configuration file containing bean definitions
-		 */
-		var applicationContext = new AnnotationConfigWebApplicationContext();
-		applicationContext.registerShutdownHook();
+	        ServletRegistration.Dynamic dispatcher = servletContext
+	                .addServlet("dispatcher", new DispatcherServlet(applicationContext));
 
+	        dispatcher.setLoadOnStartup(1);
+	        dispatcher.addMapping("/");
+			
 	}
 
 }
---
+++ 03-webconfig-based-application-context/src/main/java/org/entitylabs/ApplicationConfiguration.java
@@ -0,0 +1,8 @@
+package org.entitylabs;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+public class ApplicationConfiguration {
+
+}
